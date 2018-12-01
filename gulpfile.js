98b266f36e3503deac5f296abaa4f1194013b4a4
lixd <dandan580826@163.com>
2018-12-01 09:23:04 +0800

    完成

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
  * @Author: 李晓丹 
  * @Date: 2018-12-01 08:11:42 
  * @Last Modified by: 李晓丹
- * @Last Modified time: 2018-12-01 09:10:11
+ * @Last Modified time: 2018-12-01 09:21:48
  */
 
 var gulp = require('gulp');
@@ -12,6 +12,10 @@ var sass = require('gulp-sass');
 var minCss = require('gulp-clean-css');
 
 var server = require('gulp-webserver');
+
+var uglify = require('gulp-uglify');
+
+var babel = require('gulp-babel');
 
 var url = require('url');
 
@@ -36,7 +40,7 @@ gulp.task('watch',function(){
 
 //起服务
 gulp.task('devServer',function(){
-    return gulp.src('src')
+    return gulp.src('build')
     .pipe(server({
         port:9090,
         middleware:function(req,res,next){
@@ -53,7 +57,7 @@ gulp.task('devServer',function(){
             }else{
                 pathname = pathname === '/' ? 'index.html' : pathname;
 
-                res.end(fs.readFileSync(path.join(__dirname,'src',pathname)));
+                res.end(fs.readFileSync(path.join(__dirname,'build',pathname)));
             }
         }
     }))
@@ -61,5 +65,37 @@ gulp.task('devServer',function(){
 
 //开发环境
 gulp.task('dev',gulp.series('devScss','devServer','watch'))
+
+//线上环境
+
+//css
+gulp.task('bCss',function(){
+    return gulp.src('./src/css/*.css')
+    .pipe(gulp.dest('./build/css'))
+})
+
+//js
+
+gulp.task('bUglify',function(){
+    return gulp.src('./src/js/*.js')
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'))
+})
+
+gulp.task('bCopyJs',function(){
+    return gulp.src('./src/js/libs/*.js')
+    .pipe(gulp.dest('./build/js/libs'))
+})
+
+gulp.task('bHtml',function(){
+    return gulp.src('./src/*.html')
+    .pipe(gulp.dest('./build'))
+})
+
+//线上环境
+gulp.task('build',gulp.parallel('bCss','bUglify','bCopyJs','bHtml'))
 
 
